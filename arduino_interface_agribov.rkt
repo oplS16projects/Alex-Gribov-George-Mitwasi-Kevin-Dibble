e8861de8692d326d3d4d4486cc8982ce5a4b9080
agribov <alexander_gribov@student.uml.edu>
2016-04-22 15:17:49 -0400

    Updated Arduino interface
    Fixed functions to get data from Arduino, and added function to control lights. Updated labeling of elements, and added more buttons/lights.

--- arduino_interface_agribov.rkt
+++ arduino_interface_agribov.rkt
@@ -5,27 +5,66 @@
 ;; WRitten by Alexander Gribov
 ;; Reading potentiometer and button inputs on arduino, and outputting signals to LED's, for Lambda looper
 
-;; Digital pins
-(define led1 2)
-(define led2 3)
-(define led3 4)
-(define led4 5)
-(define butA 6)
-(define butB 7)
+;; Define digital pins
+(define led0 2)
+(define led1 3)
+(define led2 4)
+(define led3 5)
+(define but0 6)
+(define but1 7)
+(define but2 8)
+(define but3 9)
 
-;; Analog pins
+;; Define analog pins
 (define potA 2)
 (define potB 3)
 
+;; Define groups
+(define buttons
+  (list but0 but1 but2 but3)
+  )
+
+(define lights
+  (list led0 led1 led2 led3)
+  )
+
+;; Helper functions
+;; getButtonData inputs a list of buttons, and outputs a list of their on/off data
+(define (getButtonData buttonList)
+  (map (λ (button) (if (= (digital-read button) LOW) 1 0)) buttonList)
+  )
+
+;; Inputs a list of knobs, and outputs a list of their positions
+(define (getKnob knobList index)
+  (if (= index 0)
+      (analog-read (car knobList))
+      (getKnob (cdr knobList) (- index 1))
+      )
+  )
+
+;; Inputs a list of lights, and then at least one index, turns on the lights referenced by that index (0-n)
+(define (lightsOn lights ind . rest)
+  (digital-write (list-ref lights ind) HIGH)
+  (if (not (null? rest))
+      (lightsOn lights (car rest) (cdr rest))
+      0
+      )
+  )
+
+;; This function initializes ASIP and communication with the board
 (define initializeBoard 
   (λ ()
     (open-asip)  
     ;; Setting 3 pins to OUTPUT_MODE
+    (set-pin-mode led0 OUTPUT_MODE)
     (set-pin-mode led1 OUTPUT_MODE)
     (set-pin-mode led2 OUTPUT_MODE)
+    (set-pin-mode led3 OUTPUT_MODE)
     
-    (set-pin-mode butA INPUT_PULLUP_MODE)
-    (set-pin-mode butB INPUT_PULLUP_MODE)
+    (set-pin-mode but0 INPUT_PULLUP_MODE)
+    (set-pin-mode but1 INPUT_PULLUP_MODE)
+    (set-pin-mode but2 INPUT_PULLUP_MODE)
+    (set-pin-mode but3 INPUT_PULLUP_MODE)
     ;(set-pin-mode potA INPUT_MODE)
     ;(set-pin-mode potB INPUT_MODE)
 
@@ -34,7 +73,7 @@
     ;; Their value is reported every 50 ms
   
     ;; Turning the three pins off
-    (map (λ (x) (digital-write x HIGH)) (list led1 led2))
+    (map (λ (x) (digital-write x LOW)) (list led1 led2 led3 led0))
     
     ) ;; end of lambda
   ) ;; end of setup
@@ -44,9 +83,9 @@
 
 (define boardCtrl
   (λ ()
-    (set! curInput (digital-read butA))
+    (set! curInput (digital-read but0))
     (cond ( (not (equal? curInput oldInput))
-        (cond ( (equal? HIGH (digital-read butA)) ;;LOW means the button is pushed in PULLUP mode (it is grounded).
+        (cond ( (equal? HIGH (digital-read but0)) ;;LOW means the button is pushed in PULLUP mode (it is grounded).
             (digital-write led1 HIGH)
             (sleep (+ 0.1 (/ (analog-read potB) 1000)))
             (digital-write led1 LOW)
@@ -56,10 +95,13 @@
             (digital-write led3 HIGH)
             (sleep (+ 0.1 (/ (analog-read potB) 1000)))
             (digital-write led3 LOW)
+            (digital-write led0 HIGH)
+            (sleep (+ 0.1 (/ (analog-read potB) 1000)))
+            (digital-write led0 LOW)
             (lightCtrl)
             )
               (else
-               (map (λ (x) (digital-write x HIGH)) (list led1 led2 led3 led4)))
+               (map (λ (x) (digital-write x HIGH)) (list led1 led2 led3 led0)))
           )
         )
       )
@@ -69,22 +111,11 @@
   )
 )
 
-(define (getButtonData Buttonlist)
-  (map (λ (button) (digital-read button)) buttonList)
-  )
-
-(define (getKnob knobList index)
-  (if (= index 0)
-      (analog-read (car knobList))
-      (getKnob (cdr knobList) (- index 1))
-      )
-  )
-
 ;; Original control function
 (define lightCtrl
   ;;(set! curInput (digital-read inputPin))
   (if (not (equal? curInput oldInput))
-      (if (equal? HIGH (digital-read butA))
+      (if (equal? HIGH (digital-read but0))
           (λ ()
             (digital-write led1 (analog-read potA))
             (sleep (+ 0.1 (/ (analog-read potB) 1000)))
@@ -104,4 +135,5 @@
   )
 
 (initializeBoard)
-(boardCtrl)
+;(lightsOn lights 1)
+;;(boardCtrl)
